588e38812b01e9095cff106eee7fa3f25b8e51f5
AMKH <alimahmoodikharil@proton.me>
2025-01-20 12:07:13 +0330

    space add to sh 15

--- 15.hexdscript.sh
+++ 15.hexdscript.sh
@@ -3,6 +3,6 @@
 echo "Enter Hex number of your choice: "
 read Hex
 
-echo -n "The decimal Value of $Hex is"
+echo -n "The decimal Value of $Hex is  "
 
-echo "obase=10; ibase=16; $Hex" | bc
+echo "obase=10; ibase=16;  $Hex" | bc
